ebb05a4c22db56d0f327e2ea733c43fb5dd3c437
LuisCampos95 <luis.fernandocampos95@gmail.com>
2022-02-08 23:13:08 -0300

    Update da infra

--- usando-remote-state/ec2.tf
+++ usando-remote-state/ec2.tf
@@ -1,9 +1,21 @@
-resource "aws_instance" "web" {
-  ami           = var.ami
-  instance_type = var.instance_type
+resource "aws_key_pair" "my_key" {
+  key_name   = "key teste"
+  public_key = file(pathexpand("~/.ssh/id_rsa.pub"))
+}
 
-  tags = {
-    Name = "Remote State"
-    Env  = "dev"
-  }
+resource "aws_instance" "ec2" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.my_key.key_name
+  vpc_security_group_ids      = [aws_security_group.web.id]
+  subnet_id                   = aws_subnet.this["sub_a"].id
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+                  #!/bin/bash
+                  sudo yum update -y
+                  sudo yum install nginx -y 
+                  sudo service nginx start
+                EOF
+  tags      = merge(local.common_tags, { Name = "Nginx Instance" })
 }
