46a446c24c8a85a84b8f24a2761c984095268155
LuisCampos95 <luis.fernandocampos95@gmail.com>
2022-02-15 01:05:26 -0300

    Update doc

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -4,24 +4,12 @@ resource "aws_key_pair" "my_key" {
   public_key = file(pathexpand("~/.ssh/id_rsa.pub"))
 }
 
-# Criação da instância EC2 com nginx
-# resource "aws_instance" "this" {
-#   ami                         = var.ami
-#   instance_type               = var.instance_type
-#   key_name                    = aws_key_pair.my_key.key_name
-#   vpc_security_group_ids      = [aws_security_group.sg.id]
-#   subnet_id                   = aws_subnet.subnet["sub_a"].id
-#   tags                        = merge(local.common_tags, { Name = "Nginx Instance" })
-#   associate_public_ip_address = true
-#   user_data                   = filebase64("nginx.sh")
-# }
-
 module "aws_instance_ec2" {
-  source                      = "./module_ec2"
-  ami                         = var.ami
-  instance_type               = var.instance_type
-  key_name                    = aws_key_pair.my_key.key_name
-  vpc_security_group_ids      = [aws_security_group.sg.id]
-  subnet_id                   = aws_subnet.subnet["sub_a"].id
-  user_data                   = file("nginx.sh")
+  source                 = "./module_ec2"
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.my_key.key_name
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  subnet_id              = aws_subnet.subnet["sub_a"].id
+  user_data              = file("nginx.sh")
 }
